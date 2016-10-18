f8fc26c28ef12eec53fe9cc1c2befdd96da28ce9
Elton Augusto Tamilheiro <elton.augusto.tamilheiro@hotmail.com>
2016-10-17 22:36:27 -0200

    finalizando

--- Material.Infra/Persistencia/Initializer.cs
+++ Material.Infra/Persistencia/Initializer.cs
@@ -8,8 +8,20 @@ namespace Material.Infra.Persistencia
     {
         protected override void Seed(MaterialContext context)
         {
-            context.Produtos.Add(new Produto("Notebook Dell 5000 SERIES", "Notebook Dell 5000 SERIES i7 16gb de RAM"));
-            context.Produtos.Add(new Produto("Mouse Microsoft Basic", "Mouse Microsoft Basic preto sem fio"));
+            var lista = new List<Produto>();
+
+            lista.Add(new Produto("Notebook Dell 5000 SERIES", "Notebook Dell 5000 SERIES i7 16gb de RAM"));
+            lista.Add(new Produto("Mouse Microsoft Basic", "Mouse Microsoft Basic preto sem fio"));
+            lista.Add(new Produto("Notebook Dell 9000 SERIES", "Notebook Dell 5000 SERIES i7 16gb de RAM"));
+            lista.Add(new Produto("Mouse Microsoft Vasic", "Mouse Microsoft Basic preto sem fio"));
+            lista.Add(new Produto("Notebook Dell 1000 SERIES", "Notebook Dell 5000 SERIES i7 16gb de RAM"));
+            lista.Add(new Produto("Mouse Microsoft Xasic", "Mouse Microsoft Basic preto sem fio"));
+            lista.Add(new Produto("Notebook Dell 99000 SERIES", "Notebook Dell 5000 SERIES i7 16gb de RAM"));
+            lista.Add(new Produto("Mouse Microsoft Yasic", "Mouse Microsoft Basic preto sem fio"));
+
+            lista.ForEach(x => x.AlterarPreco(10));
+            lista.ForEach(x => context.Produtos.Add(x));
+
             context.SaveChanges();
 
             base.Seed(context);
